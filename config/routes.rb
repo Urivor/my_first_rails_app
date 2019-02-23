2acd2fdfaff9a2b2f617bfd335c49488ec8218c9
Nelson <neilmenjivar@gmail.com>
2019-02-23 15:38:07 -0600

    Returned routes.rb back to default

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-Rails.application.routes.draw do
-
-  resources :cars
-end
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
